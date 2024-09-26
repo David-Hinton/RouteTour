f7590b1a57c258946faa696d1a83e386e3d107e3
David Hinton <david.paul.hinton@proton.me>
2024-09-26 13:30:51 -0400

    updating to use panorama instead of static image

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+function initialize() {
+    const fenway = { lat: 42.345573, lng: -71.098326 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: fenway,
+      zoom: 14,
+    });
+    const panorama = new google.maps.StreetViewPanorama(
+      document.getElementById("pano"),
+      {
+        position: fenway,
+        pov: {
+          heading: 34,
+          pitch: 10,
+        },
+      },
+    );
+  
+    map.setStreetView(panorama);
+  }
+  
+  window.initialize = initialize;
