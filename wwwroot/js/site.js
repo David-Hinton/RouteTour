27438f7bd06548ee4a8d4b18cc3fd8d0a79a66d2
David Hinton <david.paul.hinton@proton.me>
2024-09-26 20:51:19 -0400

    adding KML layer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,11 +8,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 function initialize() {
-    const fenway = { lat: 42.345573, lng: -71.098326 };
+    const fenway = { lat: 40.4406, lng: -79.9959 };
     const map = new google.maps.Map(document.getElementById("map"), {
       center: fenway,
-      zoom: 14,
+      zoom: 12,
     });
+
+    const ctaLayer = new google.maps.KmlLayer({
+        url: "https://file.io/bI4o7fkRk0NY",
+        map: map,
+      });
+
     const panorama = new google.maps.StreetViewPanorama(
       document.getElementById("pano"),
       {
@@ -23,7 +29,7 @@ function initialize() {
         },
       },
     );
-  
+
     map.setStreetView(panorama);
   }
   
